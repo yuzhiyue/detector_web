d36dd8e8604714a7527306bd005cc13e28d332b3
cosineyu <yzycn@qq.com>
2016-06-18 13:45:23 +0800

    feature search

--- app/feature_page.jsx
+++ app/feature_page.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import Comm from './comm.jsx'
 
+var SearchBar = React.createClass({
+    handleChange: function (e) {
+        console.log("handleChange:"+e.target.value)
+        this.setState({input:e.target.value});
+    },
+    handleClick:function () {
+        this.props.handleSearch(this.state.input)
+    },
+    getInitialState: function() {
+        console.log("getInitialState")
+        return  {input:""};
+    },
+    render: function() {
+        return (
+            <div>
+                <div className="input-group">
+                    <input type="text" className="form-control" placeholder="输入MAC地址或手机号"  onChange={this.handleChange} />
+                    <span className="input-group-btn">
+                        <button className="btn btn-default" type="button" onClick={this.handleClick} >查询</button>
+                    </span>
+                </div>
+            </div>
+        );
+    }
+});
+
 var FeatureRow = React.createClass({
     render: function () {
         return (
@@ -31,8 +57,26 @@ var Panel = React.createClass({
 });
 
 var FeaturePage = React.createClass({
+    handleSearch: function (value) {
+        this.search_value = value
+        console.log("handleSearch:" + value)
+        console.log("loadTraceFromServer")
+        var url =  Comm.server_addr + '/feature/query?request={"phone":"' + value + '","mac":"' + value + '"}'
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            cache: false,
+            success: function (rsp) {
+                console.log("loadFeatureFromServer response", rsp)
+                this.setState({search_data:rsp});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(url, status, err.toString());
+            }.bind(this)
+        });
+    },
     getInitialState: function () {
-        return {data:{total_feature_num:0, today_update_feature_num:0, last_update_feature:[]}}
+        return {data:{total_feature_num:0, today_update_feature_num:0, last_update_feature:[]},search_data:{feature_list:[]}}
     },
     componentDidMount: function () {
         var url = Comm.server_addr + '/feature/summary'
@@ -51,22 +95,27 @@ var FeaturePage = React.createClass({
     },
     render: function () {
         var rows = []
-        this.state.data.last_update_feature.forEach(function (feature) {
-            var time = feature.time
-            var date = new Date()
-            date.setTime(time * 1000)
-            var dateString = date.toLocaleString()
+        if (this.state.search_data.feature_list != null) {
+            this.state.search_data.feature_list.forEach(function (feature) {
+                var time = feature.time
+                var date = new Date()
+                date.setTime(time * 1000)
+                var dateString = date.toLocaleString()
                 rows.push(<FeatureRow mac={feature.mac} phone={feature.phone} idType="" idNo="" time={dateString} orgCode={feature.org_code}/>)
-        });
+            });
+        }
         return (
             <div className="container-fluid page-container">
                 <div className="row">
                     <div className="col-sm-6"><Panel title="特征库总数" body={this.state.data.total_feature_num} linkText="查看列表"></Panel></div>
                     <div className="col-sm-6"><Panel title="今日更新数" body={this.state.data.today_update_feature_num} linkText="查看列表"></Panel></div>
                 </div>
+                <div className="row" style={{width:"300px"}}>
+                    <SearchBar handleSearch={this.handleSearch} ></SearchBar>
+                </div>
                 <div className="row">
                     <div className="panel panel-primary">
-                        <div className="panel-heading">最新入库特征</div>
+                        <div className="panel-heading">特征库搜索结果</div>
                         <table className="table table-striped">
                             <thead>
                             <tr>
